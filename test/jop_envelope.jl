f63148523433ec48ce1bcec155a971455127141b
samtkaplan <sam.kaplan@chevron.com>
2019-05-17 16:13:26 -0500

    jacobian->jacobian!

--- test/jop_envelope.jl
+++ test/jop_envelope.jl
@@ -16,7 +16,7 @@ end
     T=Float64
     pwr = 0.25
     F = JopEnvelope(JetSpace(T,N), pwr)
-    J  = jacobian(F, -1 .+ 2 .* rand(domain(F)))
+    J  = jacobian!(F, -1 .+ 2 .* rand(domain(F)))
 
     #  check: J m1 + J m2 - J m3 = J (m1 + m2 - m3)
     m1 = -1 .+ 2 .* rand(domain(F))
@@ -33,7 +33,7 @@ end
 
 @testset "Envelope^($(power)) dot product test, dimension=$(length(N)), T=$(T)" for power in (0.25, 0.5, 1.0), T in (Float64,Float32), N in ( (n1,n2), (n1,n2,n3) )
     F = JopEnvelope(JetSpace(T,N))
-    J  = jacobian(F, -1 .+ 2 .* rand(domain(F)))
+    J  = jacobian!(F, -1 .+ 2 .* rand(domain(F)))
     lhs, rhs = dot_product_test(J, -1 .+ 2 .* rand(domain(F)), -1 .+ 2 .* rand(range(F)))
     @test lhs ≈ rhs
 end
