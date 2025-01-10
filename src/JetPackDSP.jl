cce2ad6ce063fd54b712993a7f5499d8e8808e3d
miladbader <milad.bader@chevron.com>
2025-01-10 14:53:02 +0000

    Add cubic B-spline interpolation (smoothing) operator

--- src/JetPackDSP.jl
+++ src/JetPackDSP.jl
@@ -1,9 +1,10 @@
 module JetPackDSP
 
-using DSP, FFTW, Jets
+using DSP, FFTW, Jets, Base.Threads
 
 include("jop_convolve.jl")
 include("jop_envelope.jl")
 include("jop_filter.jl")
+include("jop_CubicSpline.jl")
 
 end
